1100632bf4ea3d6252ec90055c6ebcb4d6f9db87
rgehm <rgehm@uni-kl.de>
2017-04-24 11:36:57 +0200

    Test

--- RoadBuilder/Form1.cs
+++ RoadBuilder/Form1.cs
@@ -12,9 +12,24 @@ namespace RoadBuilder
 {
     public partial class Form1 : Form
     {
+        Graphics g;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+            for (int i = 0; i < this.Width; i += Convert.ToInt32(textBox1.Text))
+            {
+                g.DrawLine(Pens.Black, i, 0, i, this.Height);
+            }
+            for (int i = 0; i < this.Height; i += Convert.ToInt32(textBox2.Text))
+            {
+                g.DrawLine(Pens.Black, 0, i, this.Width, i);
+            }
+            g.Dispose();
         }
     }
 }
